1858a6a6060aba0fa79ddabb295f9d551e7ddfdb
Simone Sestito <simone@simonesestito.com>
2020-04-20 19:38:15 +0200

    Implement example route with POST JSON body

--- src/controller/ExampleController.php
+++ src/controller/ExampleController.php
@@ -4,6 +4,7 @@ class ExampleController extends BaseController {
     public function __construct() {
         $this->registerRoute('/:name', 'GET', NULL, 'hello');
         $this->registerRoute('', 'GET', NULL, 'err');
+        $this->registerRoute('', 'POST', null, 'echoBody');
     }
 
     public static function getBaseUrl(): string {
@@ -14,6 +15,11 @@ class ExampleController extends BaseController {
         return 'Hello, '.$name;
     }
 
+    public function echoBody(NewUser $body) {
+        return $body;
+    }
+
+    /** @noinspection PhpUnhandledExceptionInspection */
     public function err() {
         throw new AppHttpException(418);
     }
