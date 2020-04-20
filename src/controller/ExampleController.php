992b0043e7dd4e212ab923fe154ee3cf4e4cca9c
Simone Sestito <simone@simonesestito.com>
2020-04-20 13:02:48 +0200

    Create ExampleController

---
+++ src/controller/ExampleController.php
@@ -0,0 +1,22 @@
+<?php
+
+class ExampleController extends BaseController {
+    public function __construct() {
+        $this->registerRoute('/:name', 'GET', NULL, 'hello');
+        $this->registerRoute('', 'GET', NULL, 'err');
+    }
+
+    public static function getBaseUrl(): string {
+        return '/hello';
+    }
+
+    public function hello($name) {
+        return 'Hello, '.$name;
+    }
+
+    public function err() {
+        throw new AppHttpException(418);
+    }
+}
+
+registerController(ExampleController::class);
