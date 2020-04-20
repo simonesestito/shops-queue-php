2d536e14cea360dd59c344a8bd58a9f30e848f08
Simone Sestito <simone@simonesestito.com>
2020-04-20 17:16:13 +0200

    Get controller instance from DI

--- src/index.php
+++ src/index.php
@@ -8,23 +8,13 @@ requireAll();
 $path = rtrim($_SERVER['PATH_INFO'], '/');
 
 try {
-    $controller = instantiateController($path);
+    $controllerClass = findController($path);
+    /** @var $controller BaseController */
+    $controller = getInstanceOf($controllerClass);
     $subUrl = str_replace($controller::getBaseUrl(), '', $path);
     $result = $controller->handleRequest($subUrl);
     header('Content-Type: application/json');
     echo json_encode($result);
 } catch (AppHttpException $e) {
     http_response_code($e->getHttpStatus());
-}
-
-/**
- * Instantiate a controller which matches the given path
- * @param $path string Path of the request
- * @return BaseController
- * @throws AppHttpException
- */
-function instantiateController($path): BaseController {
-    $controllerClass = findController($path);
-    // TODO Get instance from DI
-    return new $controllerClass;
 }
