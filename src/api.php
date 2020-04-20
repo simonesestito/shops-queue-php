1d41c064d274301d9e00b20ae17d8ddad6556ea3
Simone Sestito <simone@simonesestito.com>
2020-04-20 11:38:44 +0200

    Import project in PhpStorm

--- src/api.php
+++ src/api.php
@@ -3,3 +3,5 @@ require_once __DIR__ . '/utils.php';
 require_all();
 
 echo $_SERVER['PATH_INFO'];
+echo "\n";
+echo Validator::class;
--- src/utils.php
+++ src/utils.php
@@ -4,6 +4,7 @@ function require_all($path = __DIR__) {
     $scan = glob("$path/*");
     foreach ($scan as $path) {
         if (preg_match('/\.php$/', $path)) {
+            /** @noinspection PhpIncludeInspection */
             require_once $path;
         } elseif (is_dir($path)) {
             require_all($path);
