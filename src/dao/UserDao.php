1fef11702fe838e00f7e5447ffde6e969e59dc67
Simone Sestito <simone@simonesestito.com>
2020-04-21 19:23:21 +0200

    UserDao: allow to find a user by email address

--- src/dao/UserDao.php
+++ src/dao/UserDao.php
@@ -2,6 +2,11 @@
 
 
 class UserDao extends Dao {
+    /**
+     * Insert a new user
+     * @param NewUser $newUser
+     * @throws DuplicateEntityException
+     */
     public function insertNewUser(NewUser $newUser) {
         $this->query("INSERT INTO User (name, surname, email, password) VALUES (?, ?, ?, ?)", [
             $this->sanitize($newUser->name),
@@ -9,5 +14,23 @@ class UserDao extends Dao {
             $this->sanitize($newUser->email),
             $this->sanitize(password_hash($newUser->password, PASSWORD_BCRYPT))
         ]);
+    }
+
+    /**
+     * Find a user by his email address
+     * @param string $email
+     * @return array|null Single associative array
+     */
+    public function getUserByEmail(string $email) {
+        $sql = "SELECT User.*, Role.name AS roleName
+                FROM User, Role
+                WHERE User.roleId = Role.id
+                AND email = ?";
+        $records = $this->query($sql, [$email]);
+        if (empty($records)) {
+            return null;
+        } else {
+            return $records[0];
+        }
     }
 }
