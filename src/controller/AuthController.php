a41f144e5628e5b3327f1b52a0cc0de5cb98dfa9
Simone Sestito <simone@simonesestito.com>
2020-04-21 19:25:55 +0200

    Add /login endpoint

--- src/controller/AuthController.php
+++ src/controller/AuthController.php
@@ -5,10 +5,13 @@ require_once __DIR__ . '/BaseController.php';
 
 class AuthController extends BaseController {
     private $userDao;
+    private $authService;
 
-    public function __construct(UserDao $userDao) {
+    public function __construct(UserDao $userDao, AuthService $authService) {
         $this->userDao = $userDao;
+        $this->authService = $authService;
         $this->registerRoute('/signup', 'POST', null, 'signupUser');
+        $this->registerRoute('/login', 'POST', null, 'login');
     }
 
     public static function getBaseUrl(): string {
@@ -18,6 +21,10 @@ class AuthController extends BaseController {
     public function signupUser(NewUser $newUser) {
         $this->userDao->insertNewUser($newUser);
         // TODO Login and return
+    }
+
+    public function login(UserLogin $userLogin): AuthResponse {
+        return $this->authService->login($userLogin);
     }
 }
 
