07ea5a14b9cd0cbf5c79a7aaf3b16d288131eafd
Simone Sestito <simone@simonesestito.com>
2020-04-21 18:11:26 +0200

    Remove unused method from ModelValidationException

--- src/exceptions/ModelValidationException.php
+++ src/exceptions/ModelValidationException.php
@@ -1,18 +1,7 @@
 <?php
 
-class ModelValidationException extends Exception {
-    private $field;
-
+class ModelValidationException extends RuntimeException {
     public function __construct(string $field) {
         parent::__construct("Error validating field $field");
-        $this->field = $field;
-    }
-
-    /**
-     * Get the field which caused this exception to be thrown
-     * @return string Target field
-     */
-    public function getField(): string {
-        return $this->field;
     }
 }
