074345b383b2e6d6b5dd551bf19953b1474953f1
Simone Sestito <simone@simonesestito.com>
2020-04-21 19:22:03 +0200

    Add Session table to SQL Database

--- db.sql
+++ db.sql
@@ -34,10 +34,10 @@ CREATE TABLE User
     email    VARCHAR(255) NOT NULL,
     -- BCrypt hash length
     password VARCHAR(60)  NOT NULL,
-    roleId   INT          NOT NULL    DEFAULT 1,
+    roleId   INT          NOT NULL DEFAULT 1,
     -- If the user is a shop owner, this will referenece that shop
     -- One-to-one relation
-    shopId   INT REFERENCES Shop (id) DEFAULT NULL,
+    shopId   INT                   DEFAULT NULL,
     UNIQUE (shopId),
     UNIQUE (email),
     FOREIGN KEY (roleID) REFERENCES Role (id)
@@ -47,13 +47,29 @@ DROP TABLE IF EXISTS Booking;
 CREATE TABLE Booking
 (
     id        INT  NOT NULL AUTO_INCREMENT PRIMARY KEY,
-    userId    INT  NOT NULL REFERENCES User (id),
-    shopId    INT  NOT NULL REFERENCES Shop (id),
+    userId    INT  NOT NULL,
+    shopId    INT  NOT NULL,
     createdAt DATE NOT NULL,
     -- Ensure a user can only have one booking per shop
     UNIQUE (userId, shopId),
     FOREIGN KEY (userId) REFERENCES User (id),
     FOREIGN KEY (shopId) REFERENCES Shop (id)
+);
+
+DROP TABLE IF EXISTS Session;
+CREATE TABLE Session
+(
+    id                     INT         NOT NULL AUTO_INCREMENT PRIMARY KEY,
+    userId                 INT         NOT NULL,
+    accessToken            VARCHAR(24) NOT NULL,
+    accessTokenExpiration  DATE        NOT NULL,
+    refreshToken           VARCHAR(44) NOT NULL,
+    refreshTokenExpiration DATE        NOT NULL,
+    -- Ensure a token cannot be used by more than 1 user,
+    -- even if that's almost impossible, but not 100% impossible
+    UNIQUE (accessToken),
+    UNIQUE (refreshToken),
+    FOREIGN KEY (userId) REFERENCES User (id)
 );
 
 SET FOREIGN_KEY_CHECKS = 1;
