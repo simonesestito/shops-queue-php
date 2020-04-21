01863c5e78a3bbf8c8b116d3bb7aaf9d9a9134ff
Simone Sestito <simone@simonesestito.com>
2020-04-21 17:53:44 +0200

    Add dev environment variables

---
+++ src/env.php
@@ -0,0 +1,11 @@
+<?php
+
+/*
+ * Environment variables.
+ * This file specifically contains dev env variables
+ */
+
+define('DB_HOST', 'localhost');
+define('DB_USERNAME', 'root');
+define('DB_PASSWORD', 'root');
+define('DB_NAME', 'shops_queue');
