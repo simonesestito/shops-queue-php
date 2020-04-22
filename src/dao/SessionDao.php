332216798381e5fcf83ebfd1fc8d4c1568a05c89
Simone Sestito <simone@simonesestito.com>
2020-04-22 08:29:17 +0200

    Retrieve session + user info by access token

--- src/dao/SessionDao.php
+++ src/dao/SessionDao.php
@@ -17,4 +17,20 @@ class SessionDao extends Dao {
         $sessionData['id'] = $id;
         return $sessionData;
     }
+
+    /**
+     * Get details about the user associated with the given access token, if any.
+     * @param string $accessToken
+     * @return mixed Session record with user and role info
+     */
+    public function getSessionByAccessToken(string $accessToken) {
+        $sql = "SELECT User.*,
+                Role.name AS role
+                FROM Session
+                JOIN User on Session.userId = User.id
+                JOIN Role on User.roleId = Role.id
+                WHERE accessToken = ? AND accessTokenExpiration > CURRENT_DATE()";
+        $records = $this->query($sql, [$accessToken]);
+        return @$records[0];
+    }
 }
