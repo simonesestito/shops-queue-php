f0c0ff1f72ebfbbaa71470ef6027e44461f38e73
Simone Sestito <simone@simonesestito.com>
2020-04-30 17:17:21 +0200

    Add endpoints /shops/:id and /shops/me

--- src/controller/ShopController.php
+++ src/controller/ShopController.php
@@ -27,6 +27,8 @@ class ShopController extends BaseController {
         $this->registerRoute('/shops', 'GET', 'ADMIN', 'listShops');
         $this->registerRoute('/shops', 'POST', 'ADMIN', 'addNewShop');
         $this->registerRoute('/shops/nearby', 'GET', '*', 'findNearShops');
+        $this->registerRoute('/shops/me', 'GET', 'OWNER', 'getOwnShop');
+        $this->registerRoute('/shops/:id', 'GET', '*', 'getShopById');
         $this->registerRoute('/shops/:id', 'PUT', 'ADMIN', 'updateShop');
         $this->registerRoute('/shops/:id', 'DELETE', 'ADMIN', 'deleteShop');
     }
@@ -96,10 +98,38 @@ class ShopController extends BaseController {
     }
 
     /**
+     * Get the shop associated with the current shop owner
+     * @return Shop
+     * @throws AppHttpException
+     */
+    public function getOwnShop() {
+        $shopId = AuthService::getAuthContext()['shopId'];
+        return $this->getShopById($shopId);
+    }
+
+    /**
+     * Get a shop by its ID
+     * @param int $id
+     * @return Shop Found shop
+     * @throws AppHttpException
+     */
+    public function getShopById(int $id) {
+        $authContext = AuthService::getAuthContext();
+        if ($authContext['shopId'] !== $id && $authContext['role'] !== 'ADMIN') {
+            throw new AppHttpException(HTTP_NOT_AUTHORIZED);
+        }
+
+        $entity = $this->shopDao->getShopById($id);
+        if ($entity === null)
+            throw new AppHttpException(HTTP_NOT_FOUND);
+        return new Shop($entity);
+    }
+
+    /**
      * Update an existing shop
      * @param $id int
      * @param NewShop $newShop
-     * @return Shop
+     * @return Shop Updated shop
      * @throws AppHttpException
      */
     public function updateShop(int $id, NewShop $newShop) {
