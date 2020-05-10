7e6e5b0ce0582fbf5714deeac0b2b18ba9aa964b
Simone Sestito <simone@simonesestito.com>
2020-05-10 13:00:04 +0200

    Add endpoint POST /users/me/fcm

---
+++ src/controller/FcmController.php
@@ -0,0 +1,44 @@
+<?php
+/**
+ * Copyright 2020 Simone Sestito
+ * This file is part of Shops Queue.
+ *
+ * Shops Queue is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU Affero General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * Shops Queue is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU Affero General Public License for more details.
+ *
+ * You should have received a copy of the GNU Affero General Public License
+ * along with Shops Queue.  If not, see <http://www.gnu.org/licenses/>.
+ */
+
+class FcmController extends BaseController {
+    private $fcmDao;
+
+    public function __construct(FcmDao $fcmDao) {
+        $this->fcmDao = $fcmDao;
+        $this->registerRoute('/users/me/fcm', 'POST', '*', 'addFcmToken');
+    }
+
+    /**
+     * Add a new FCM token to the current user
+     * @param FcmToken $fcmToken
+     */
+    public function addFcmToken(FcmToken $fcmToken) {
+        $userId = AuthService::getAuthContext()['id'];
+
+        // Delete token if already assigned (maybe to another user)
+        $this->fcmDao->deleteToken($fcmToken->token);
+
+        $this->fcmDao->addToken($userId, $fcmToken->token);
+    }
+}
+
+onInit(function () {
+    registerController(FcmController::class);
+});
